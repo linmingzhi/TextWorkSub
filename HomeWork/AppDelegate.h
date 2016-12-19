5a503d442f93e9fb33af77a62eb07c28ac97df69
linmingzhi987 <linmingzhi987@126.com>
2016-12-19 13:33:42 +0800

    Initial Commit

---
+++ HomeWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeWork
+//
+//  Created by 悦厚 on 19/12/16.
+//  Copyright © 2016年 悦厚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeWork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeWork
+//
+//  Created by 悦厚 on 19/12/16.
+//  Copyright © 2016年 悦厚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
